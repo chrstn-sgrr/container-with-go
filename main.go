4ae6408058223f3120fc864651d5d7c54d10cc96
chrstn-sgrr <christian9esguerra@gmail.com>
2025-09-21 22:05:13 +0800

    feat: root filesystem

--- main.go
+++ main.go
@@ -34,6 +34,11 @@ func parent() {
 }
 
 func child() {
+	must(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
+	must(os.MkdirAll("rootfs/oldrootfs", 0700))
+	must(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+	must(os.Chdir("/"))
+
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
